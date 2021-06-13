f6cb5f5eb85b26700be8d34c57c962edce0004eb
Carlos Molina <cmmolinas01@gmail.com>
2021-06-13 17:32:41 +0200

    VDS1022: Added new methods

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Develop of an automatic electro-mechanic device for in-circuit testing of PCBAs, based on flying probe system.
 
 
-# CONTRIBUTORS
+## CONTRIBUTORS
 
-Pablo Martínez García (pmartine92@gmail.com)
-Carlos Manuel Molina Sotoca (cmmolinas01@gmail.com)
+- Pablo Martínez García (pmartine92@gmail.com)
+- Carlos Manuel Molina Sotoca (cmmolinas01@gmail.com)
--- source/visa_devices.py
+++ source/visa_devices.py
@@ -25,6 +25,9 @@ class VISAController:
         # For Serial and TCP/IP socket connections enable the read Termination Character, or read's will timeout
         if session.resource_name.startswith('ASRL') or session.resource_name.endswith('SOCKET'):
             session.read_termination = '\n'
+
+        # Set huge timeout for ADC reading:
+        session.timeout = 10000
         return resource_manager, session
 
     def send_command(self, scpi_command):
@@ -42,34 +45,129 @@ class VISAController:
         self.resource_manager.close()
 
 
-class OwonVDS1022:
+class OwonVDS1022(VISAController):
     def __init__(self, port=5188):
+        # Oscilloscope attributes:
         self.address = f"TCPIP0::127.0.0.1::{port}::SOCKET"
         self.pixels_per_div = 25
-        self.configuration = {"CH1": {"vertical_scale": 5,
-                                      "vertical_offset": 1.64}}
 
-        self.visa_ctrl = VISAController(self.address)
+        # VISA controller:
+        super().__init__(self.address)
 
-    def convert_pixels_to_value(self, vertical_scale, vertical_offset, pixels):
-        return vertical_scale * ((pixels - vertical_offset * self.pixels_per_div) / self.pixels_per_div)
+    def convert_pixels_to_value(self, vertical_scale, offset_per_div, pixels):
+        return vertical_scale * ((pixels - offset_per_div * self.pixels_per_div) / self.pixels_per_div)
 
-    def convert_value_to_pixels(self, vertical_scale, vertical_offset, value):
-        return self.pixels_per_div * (value / vertical_scale) + vertical_offset * self.pixels_per_div
+    def convert_value_to_pixels(self, vertical_scale, offset_per_div, value):
+        return self.pixels_per_div * (value / vertical_scale) + offset_per_div * self.pixels_per_div
 
-    def set_edge_trigger(self, mode="NORMAL", channel=1, slope="RISE", level=0.0):
-        vertical_scale = self.configuration[f"CH{str(channel)}"]["vertical_scale"]
-        vertical_offset = self.configuration[f"CH{str(channel)}"]["vertical_offset"]
+    def get_id(self):
+        self.send_command("*IDN?")
 
-        self.visa_ctrl.send_command(f":TRIGGER:MODE {mode}")
-        self.visa_ctrl.send_command(f":TRIGGER:SINGLE EDGE")
-        self.visa_ctrl.send_command(f":TRIGGER:SINGLE:EDGE:SOURCE CH{str(channel)}")
-        self.visa_ctrl.send_command(f":TRIGGER:SINGLE:EDGE:SLOPE {slope}")
-        self.visa_ctrl.send_command(f":TRIGGER:SINGLE:EDGE:LEVEL "
-                                    f"{self.convert_value_to_pixels(vertical_scale, vertical_offset, level)}")
+    def set_restore(self):
+        self.send_command("*RST")
+
+    def set_general_state(self, state):
+        """
+        Possible general states:
+            {RUN | STOP}
+        """
+        current_state = self.send_command("*RUNSTOP?")
+        if (current_state == "RUN" and state == "STOP") or (current_state == "STOP" and state == "RUN"):
+            self.send_command("*RUNSTOP")
+
+    def set_timebase(self, scale):
+        """
+        Scale possible values:
+            {5ns | 10ns | 20ns | 50ns | 100ns | 200ns | 500ns | 1us | 2us | 5us | 10us | 20us | 50us |
+             100us | 200us | 500us | 1ms | 2ms | 5ms | 10ms | 20ms | 50ms | 100ms | 200ms | 500ms |
+             1s | 2s | 5s | 10s | 20s | 50s | 100s}
+        """
+        self.send_command(f":TIMEBASE:SCALE {scale}")
+        self.send_command(f":TIMEBASE:HOFFSET 0")
+
+    def set_channel_state(self, channel, state):
+        """
+        Channel possible states:
+            {ON | OFF}
+        """
+        self.send_command(f":CHANNEL{channel}:DISPLAY {state}")     # ON, OFF
+
+    def set_channel_configuration(self, channel, scale, offset, coupling="DC", probe="X1"):
+        """
+        Channel possible couplings:
+            {AC | DC | GND}
+        Channel possible probe attenuation ratios:
+            {X1 | X10 | X100 | X1000}
+        Channel possible scales:
+            {5mV | 10mV | 20mV | 50mV | 100mV | 200mV | 500mV | 1V | 2V | 5V}
+        """
+        self.send_command(f":CHANNEL{channel}:COUPING {coupling}")
+        self.send_command(f":CHANNEL{channel}:PROBE {probe}")
+        self.send_command(f":CHANNEL{channel}:SCALE {scale}")
+        self.send_command(f":CHANNEL{channel}:OFFSET "
+                          f"{self.convert_value_to_pixels(scale, 0, offset)}")
+
+    def set_edge_trigger(self, channel, vertical_scale, offset_per_div, mode="NORMAL", slope="RISE", level=0.0):
+        """
+        Trigger possible modes:
+            {AUTO | NORMAL}
+        Trigger possible slopes:
+            {RISE | FALL}
+        """
+        self.send_command(f":TRIGGER:MODE {mode}")
+        self.send_command(f":TRIGGER:SINGLE EDGE")
+        self.send_command(f":TRIGGER:SINGLE:EDGE:SOURCE CH{str(channel)}")
+        self.send_command(f":TRIGGER:SINGLE:EDGE:SLOPE {slope}")  # RISE, FALL
+        self.send_command(f":TRIGGER:SINGLE:EDGE:LEVEL "
+                          f"{self.convert_value_to_pixels(vertical_scale, offset_per_div, level)}")
+
+    def set_acquire(self, acq_type="SAMPLE", count=4, n_samples="1K"):
+        """
+        Acquire possible modes:
+            {SAMPLE | AVERAGE | PEAK}
+        Acquire average values range:
+            {1 ~ 128}
+        Acquire possible values memory depth:
+            {1K | 10K | 100K | 1M | 5M or 10M}
+        """
+        self.send_command(f":ACQUIRE:TYPE {acq_type}")
+        if type == "AVERAGE":
+            self.send_command(f":ACQUIRE:AVERAGE {count}")
+        self.send_command(f":TRIGGER:MDEPTH {n_samples}")
+
+    def set_measurement(self, channel, measurement):
+        """
+        Measurement possible modes:
+            {PERiod | FREQuency | AVERage | MAX | MIN | VTOP | VBASe | VAMP | PKPK | CYCRms | RTime |
+             FTime | PDUTy | NDUTy | PWIDth | NWIDth | OVERshoot | PREShoot | RDELay | FDELay}
+        """
+        self.send_command(f":MEASURE:SOURCE CH{channel}")
+        self.send_command(f":MEASURE:ADD {measurement}")
+
+    def remove_measurement(self, channel, measurement="ALL"):
+        """
+        Measurement possible modes:
+            {PERiod | FREQuency | AVERage | MAX | MIN | VTOP | VBASe | VAMP | PKPK | CYCRms | RTime |
+             FTime | PDUTy | NDUTy | PWIDth | NWIDth | OVERshoot | PREShoot | RDELay | FDELay | ALL}
+        """
+        self.send_command(f":MEASURE:SOURCE CH{channel}")
+        self.send_command(f":MEASURE:DELETE {measurement}")
+
+    def get_measurement(self, channel, measurement):
+        """
+        Measurement possible modes:
+            {PERiod | FREQuency | AVERage | MAX | MIN | VTOP | VBASe | VAMP | PKPK | CYCRms | RTime |
+             FTime | PDUTy | NDUTy | PWIDth | NWIDth | OVERshoot | PREShoot | RDELay | FDELay}
+        """
+        self.send_command(f":MEASURE{channel}:{measurement}?")
+
+    def get_waveform(self, channel):
+        self.send_command(f"*ADC? CH{channel}")
 
 
 if __name__ == '__main__':
     osc_obj = OwonVDS1022(port=5188)
-    osc_obj.set_edge_trigger(level=2.5)
-    osc_obj.visa_ctrl.close_connection()
+    osc_obj.set_general_state("RUN")
+    # osc_obj.set_edge_trigger(channel=1, vertical_scale=5, offset_per_div=1.64, level=2.5)
+    osc_obj.get_waveform(1)
+    osc_obj.close_connection()
