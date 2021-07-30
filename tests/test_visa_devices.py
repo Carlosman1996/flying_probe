86c92d3a3b111c499233e5744fea4e223471ce62
Carlos Molina <cmmolinas01@gmail.com>
2021-07-31 00:12:20 +0200

    VDS1022: adjusted methods to architecture

--- tests/test_visa_devices.py
+++ tests/test_visa_devices.py
@@ -3,66 +3,88 @@ __email__ = "cmmolinas01@gmail.com"
 
 
 def test_vds1022_inactive(vds1022_inactive):
-    inputs = [{"channel": 1,
-               "signal_max_value": 5,
-               "signal_min_value": 0,
-               "frequency": 1000,
-               "trigger_edge_slope": "RISE",
-               "coupling": "DC",
-               "probe_attenuation": "X10",
-               "measurements": ["FREQUENCY", "CYCRMS", "MAX", "MIN"]}]
+    conf_dict = [{"channel": 1,
+                  "trigger_edge_slope": "RISE",
+                  "coupling": "DC",
+                  "probe_attenuation": "X10"}]
 
-    result = vds1022_inactive.run(inputs)
+    meas_dict = {"channel": 1,
+                 "signal_max_value": 5,
+                 "signal_min_value": 0,
+                 "frequency": 1000,
+                 "measurements": ["FREQUENCY", "CYCRMS", "MAX", "MIN"]}
 
-    assert len(result) == 1, "Only one channel was specified."
-    assert len(result[0]["measurements"].keys()) == 4, "The method must return 4 measurements."
-    for measurement in result[0]["measurements"].values():
+    vds1022_inactive.initialize(conf_dict)
+    result = vds1022_inactive.measure(meas_dict)
+    vds1022_inactive.stop()
+
+    assert len(result["measurements"].keys()) == 4, "The method must return 4 measurements."
+    for measurement in result["measurements"].values():
         assert type(measurement) == float, "The measurements must return float values."
 
     vds1022_inactive.close_connection()
 
 
 def test_vds1022_inactive_multiple_channels(vds1022_inactive):
-    inputs = [{"channel": 2,
-               "signal_max_value": 5,
-               "signal_min_value": 0,
-               "frequency": 1000,
-               "trigger_edge_slope": "RISE",
-               "coupling": "DC",
-               "probe_attenuation": "X10",
-               "measurements": ["FREQUENCY", "CYCRMS", "MAX", "MIN"]},
-              {"channel": 1,
-               "signal_max_value": 5,
-               "signal_min_value": 0,
-               "frequency": 0,
-               "trigger_edge_slope": "FALL",
-               "coupling": "AC",
-               "probe_attenuation": "X1",
-               "measurements": []}]
+    conf_dict = [{"channel": 2,
+                  "trigger_edge_slope": "RISE",
+                  "coupling": "DC",
+                  "probe_attenuation": "X10"},
+                 {"channel": 1,
+                  "trigger_edge_slope": "FALL",
+                  "coupling": "AC",
+                  "probe_attenuation": "X1"}
+                 ]
 
-    result = vds1022_inactive.run(inputs)
+    vds1022_inactive.initialize(conf_dict)
 
-    assert len(result) == 2, "Only one channel was specified."
-    assert len(result[0]["measurements"].keys()) == 4, "The method must return 4 measurements."
-    assert len(result[1]["measurements"].keys()) == 0, "The method must return 4 measurements."
-    for measurement in result[0]["measurements"].values():
+    results = []
+    meas_dict = {"channel": 2,
+                 "signal_max_value": 5,
+                 "signal_min_value": 0,
+                 "frequency": 1000,
+                 "measurements": ["FREQUENCY", "CYCRMS", "MAX", "MIN"]}
+    results.append(vds1022_inactive.measure(meas_dict))
+    meas_dict = {"channel": 1,
+                 "signal_max_value": 5,
+                 "signal_min_value": 0,
+                 "frequency": 0,
+                 "measurements": []}
+    results.append(vds1022_inactive.measure(meas_dict))
+
+    vds1022_inactive.stop()
+
+    assert len(results) == 2, "Only one channel was specified."
+    assert len(results[0]["measurements"].keys()) == 4, "The method must return 4 measurements."
+    assert len(results[1]["measurements"].keys()) == 0, "The method must return 4 measurements."
+    for measurement in results[0]["measurements"].values():
         assert type(measurement) == float, "The measurements must return float values."
 
     vds1022_inactive.close_connection()
 
 
-def test_vds1022_inactive_channel_not_valid(vds1022_inactive):
-    inputs = [{"channel": 3,
-               "signal_max_value": 5,
-               "signal_min_value": 0,
-               "frequency": 1000,
-               "trigger_edge_slope": "RISE",
-               "coupling": "DC",
-               "probe_attenuation": "X10",
-               "measurements": ["FREQUENCY", "CYCRMS", "MAX", "MIN"]}]
+def test_vds1022_inactive_channel_not_valid_conf_stage(vds1022_inactive):
+    conf_dict = [{"channel": 3,
+                  "trigger_edge_slope": "RISE",
+                  "coupling": "DC",
+                  "probe_attenuat ion": "X10"}]
 
     try:
-        vds1022_inactive.run(inputs)
+        vds1022_inactive.initialize(conf_dict)
+        assert 0, "Channel not valid."
+    except Exception as exception:
+        assert str(exception) == "Channel is not valid. Available channels are 1 and 2.", "Not valid exception error."
+
+
+def test_vds1022_inactive_channel_not_valid_meas_stage(vds1022_inactive):
+    meas_dict = {"channel": 3,
+                 "signal_max_value": 5,
+                 "signal_min_value": 0,
+                 "frequency": 0,
+                 "measurements": []}
+
+    try:
+        vds1022_inactive.measure(meas_dict)
         assert 0, "Channel not valid."
     except Exception as exception:
         assert str(exception) == "Channel is not valid. Available channels are 1 and 2.", "Not valid exception error."
