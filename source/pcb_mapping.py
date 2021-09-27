d7d62719076afd4b1f8434277daa2a2fa0a4a476
Carlosman1996 <cmmolinas01@gmail.com>
2021-09-27 23:55:21 +0200

    PCB mapping: rotation bug fixed and added PCB borders

--- source/pcb_mapping.py
+++ source/pcb_mapping.py
@@ -102,34 +102,57 @@ class PCBMappingKiCAD:
         return point_rotate
 
     def read_components(self):
-        pcb_data = FileOperations.read_file_lines(self.pcb_path)
-
-        # Read and split modules:
-        append_data = False
-        modules_data = []
-        for line in pcb_data:
-            # Module starts with the string "  (module":
-            if line[0:9] == "  (module":
-                append_data = True
-                modules_data.append([])
-
-            # If a module has been detected, the line must be appended in the corresponding group:
-            if append_data:
-                modules_data[-1].append(line)
-
-                # Module finished with the line "  )":
-                if line == "  )\n":
-                    append_data = False
-
         # Structure modules information:
-        def search_text_between_string(first_string, second_string):
+        def search_text_between_string(first_string: str, second_string: str, line_info: str):
             regex_result = re.search(f"(?<={first_string})(.*?)(?={second_string})", line_info)
             if regex_result is not None:
                 return regex_result.group(0)
             else:
                 return None
 
-        modules = {}
+        pcb_data = FileOperations.read_file_lines(self.pcb_path)
+        processed_pcb_data = {"borders": [],
+                              "modules": {}}
+
+        # Read and split PCB general info and modules:
+        append_data = False
+        modules_data = []
+        for line_info in pcb_data:
+            # Search PCB borders:
+            if line_info[0:11] == "  (gr_line ":
+                values_start = search_text_between_string(first_string="\\(start ", second_string="\\)",
+                                                          line_info=line_info)
+                values_end = search_text_between_string(first_string="\\(end ", second_string="\\)",
+                                                        line_info=line_info)
+                if values_end is not None and values_start is not None:
+                    start_point = [float(number) for number in values_start.split(" ")]
+                    end_point = [float(number) for number in values_end.split(" ")]
+
+                    # Read angle:
+                    # angle = float(search_text_between_string(first_string="\\(angle ", second_string="\\)",
+                    #                                          line_info=line_info))
+                    angle = 0
+
+                    # Rotate points before save them:
+                    values_dict = {"start": self.rotate_point_around_origin(start_point, angle),
+                                   "end": self.rotate_point_around_origin(end_point, angle)}
+                    processed_pcb_data["borders"].append(values_dict)
+                else:
+                    processed_pcb_data["borders"].append(None)
+
+            # Module starts with the string "  (module":
+            if line_info[0:10] == "  (module ":
+                append_data = True
+                modules_data.append([])
+
+            # If a module has been detected, the line must be appended in the corresponding group:
+            if append_data:
+                modules_data[-1].append(line_info)
+
+                # Module finished with the line "  )":
+                if line_info == "  )\n":
+                    append_data = False
+
         for index, module_data in enumerate(modules_data):
             reference = f"Unknown_module_{index}"
             module_info = {"placement_outlines": {"circle": [],
@@ -144,17 +167,21 @@ class PCBMappingKiCAD:
                 # Search module type and layer:
                 if line_info[0:10] == "  (module ":
                     # Find module type:
-                    module_info["type"] = search_text_between_string(first_string="module ", second_string=" \(")
+                    module_info["type"] = search_text_between_string(first_string="module ", second_string=" \\(",
+                                                                     line_info=line_info)
                     # Find module layer:
-                    module_info["layer"] = search_text_between_string(first_string="layer ", second_string="\)")
+                    module_info["layer"] = search_text_between_string(first_string="layer ", second_string="\\)",
+                                                                      line_info=line_info)
 
                 # Search module reference:
                 elif line_info[0:23] == "    (fp_text reference ":
-                    reference = search_text_between_string(first_string="reference ", second_string=" \(")
+                    reference = search_text_between_string(first_string="reference ", second_string=" \\(",
+                                                           line_info=line_info)
 
                 # Search origin coordinates:
                 if line_info[0:8] == "    (at ":
-                    module_info["position"] = search_text_between_string(first_string="\(at ", second_string="\)")
+                    module_info["position"] = search_text_between_string(first_string="\\(at ", second_string="\\)",
+                                                                         line_info=line_info)
                     module_info["position"] = [float(number) for number in module_info["position"].split(" ")]
                     # Separate rotation from position:
                     if len(module_info["position"]) == 3:
@@ -163,8 +190,10 @@ class PCBMappingKiCAD:
 
                 # Search placement outlines:
                 if line_info[0:15] == "    (fp_circle ":
-                    values_center = search_text_between_string(first_string="\(center ", second_string="\)")
-                    values_end = search_text_between_string(first_string="\(end ", second_string="\)")
+                    values_center = search_text_between_string(first_string="\\(center ", second_string="\\)",
+                                                               line_info=line_info)
+                    values_end = search_text_between_string(first_string="\\(end ", second_string="\\)",
+                                                            line_info=line_info)
                     if values_end is not None and values_center is not None:
                         center_point = [float(number) for number in values_center.split(" ")]
                         end_point = [float(number) for number in values_end.split(" ")]
@@ -176,9 +205,12 @@ class PCBMappingKiCAD:
                     else:
                         module_info["placement_outlines"]["circle"].append(None)
                 elif line_info[0:12] == "    (fp_arc ":
-                    values_start = search_text_between_string(first_string="\(start ", second_string="\)")
-                    values_end = search_text_between_string(first_string="\(end ", second_string="\)")
-                    values_angle = search_text_between_string(first_string="\(angle ", second_string="\)")
+                    values_start = search_text_between_string(first_string="\\(start ", second_string="\\)",
+                                                              line_info=line_info)
+                    values_end = search_text_between_string(first_string="\\(end ", second_string="\\)",
+                                                            line_info=line_info)
+                    values_angle = search_text_between_string(first_string="\\(angle ", second_string="\\)",
+                                                              line_info=line_info)
                     if values_start is not None and values_end is not None and values_angle is not None:
                         start_point = [float(number) for number in values_start.split(" ")]
                         end_point = [float(number) for number in values_end.split(" ")]
@@ -192,8 +224,10 @@ class PCBMappingKiCAD:
                     else:
                         module_info["placement_outlines"]["arc"].append(None)
                 elif line_info[0:13] == "    (fp_line ":
-                    values_start = search_text_between_string(first_string="\(start ", second_string="\)")
-                    values_end = search_text_between_string(first_string="\(end ", second_string="\)")
+                    values_start = search_text_between_string(first_string="\\(start ", second_string="\\)",
+                                                              line_info=line_info)
+                    values_end = search_text_between_string(first_string="\\(end ", second_string="\\)",
+                                                            line_info=line_info)
                     if values_end is not None and values_start is not None:
                         start_point = [float(number) for number in values_start.split(" ")]
                         end_point = [float(number) for number in values_end.split(" ")]
@@ -207,17 +241,19 @@ class PCBMappingKiCAD:
 
                 # Search pads:
                 if line_info[0:9] == "    (pad ":
-                    simplified_line = search_text_between_string(first_string='\(pad ', second_string=' \(at')
+                    simplified_line = search_text_between_string(first_string='\\(pad ', second_string=' \\(at',
+                                                                 line_info=line_info)
                     split_line_info = simplified_line.split(" ")
 
                     pad_number = split_line_info[0]
                     module_info["pads"][pad_number] = {"type": split_line_info[1],
                                                        "shape": split_line_info[2]}
 
-                    pad_position = search_text_between_string(first_string="\(at ", second_string="\)")
+                    pad_position = search_text_between_string(first_string="\\(at ", second_string="\\)",
+                                                              line_info=line_info)
                     module_info["pads"][pad_number]["position"] = [float(number) for number in pad_position.split(" ")]
                     # Separate rotation from position:
-                    if len(module_info["position"]) == 3:
+                    if len(module_info["pads"][pad_number]["position"]) == 3:
                         rotation = module_info["pads"][pad_number]["position"][2]
                         position = module_info["pads"][pad_number]["position"][:2]
                         module_info["pads"][pad_number]["rotation"] = rotation
@@ -226,31 +262,40 @@ class PCBMappingKiCAD:
                     else:
                         module_info["pads"][pad_number]["rotation"] = 0
 
-                    pad_size = search_text_between_string(first_string="\(size ", second_string="\)")
+                    pad_size = search_text_between_string(first_string="\\(size ", second_string="\\)",
+                                                          line_info=line_info)
                     module_info["pads"][pad_number]["size"] = [float(number) for number in pad_size.split(" ")]
 
-                    pad_drill = search_text_between_string(first_string="\(drill ", second_string="\)")
+                    pad_drill = search_text_between_string(first_string="\\(drill ", second_string="\\)",
+                                                           line_info=line_info)
                     if pad_drill is not None:
                         module_info["pads"][pad_number]["drill"] = float(pad_drill)
                     else:
                         module_info["pads"][pad_number]["drill"] = None
                 # Search nets pads:
                 elif line_info[0:11] == "      (net ":
-                    net_name = search_text_between_string(first_string='net ', second_string='\)')
+                    net_name = search_text_between_string(first_string='net ', second_string='\\)',
+                                                          line_info=line_info)
                     module_info["pads"][pad_number]["net_name"] = net_name
 
             # Append data to global dictionary:
-            modules[reference] = module_info
-        return modules
+            processed_pcb_data["modules"][reference] = module_info
+        return processed_pcb_data
 
 
 class PCBDrawing:
-    def run(self, modules):
+    def run(self, pcb_data):
         factor = 4
         image_obj = Image.new("RGB", (512 * factor, 512 * factor), (0, 0, 0))
         draw_obj = ImageDraw.Draw(image_obj)
 
-        for module in modules.values():
+        # Draw PCB general data:
+        for line in pcb_data["borders"]:
+            draw_obj.line((line["start"][0] * factor, line["start"][1] * factor,
+                           line["end"][0] * factor, line["end"][1] * factor), fill=(255, 255, 0), width=1)
+
+        # Draw modules:
+        for module in pcb_data["modules"].values():
             start_point = module["position"]
             for point in module["placement_outlines"]["line"]:
                 initial_x = point["start"][0] + start_point[0]
@@ -272,8 +317,6 @@ class PCBDrawing:
                 start_angle = math.atan2(point["start"][1], point["start"][0]) * 180 / math.pi
                 end_angle = math.atan2(point["end"][1], point["end"][0]) * 180 / math.pi
 
-                print(start_angle, end_angle, point["angle"], start_angle-end_angle)
-
                 left_up_x = start_point[0] + point["start"][0] - radius
                 left_up_y = start_point[1] + point["start"][1] - radius
                 right_down_x = start_point[0] + point["start"][0] + radius
@@ -287,7 +330,7 @@ class PCBDrawing:
                 right_down_x = start_point[0] + par_params["position"][0] + par_params["size"][0]
                 right_down_y = start_point[1] + par_params["position"][1] + par_params["size"][0]
                 draw_obj.ellipse((left_up_x * factor, left_up_y * factor, right_down_x * factor, right_down_y * factor),
-                                 fill=(255, 0, 0), width=0.1)
+                                 fill=(255, 255, 0), width=0)
 
         image_obj.show()
 
@@ -299,7 +342,7 @@ if __name__ == "__main__":
     # print(info_df)
     pcb_obj = PCBMappingKiCAD()
     pcb_info = pcb_obj.read_components()
-    print(json.dumps(pcb_info["C1"], sort_keys=True, indent=4))
+    print(json.dumps(pcb_info, sort_keys=True, indent=4))
 
     pcb_draw_obj = PCBDrawing()
     pcb_draw_obj.run(pcb_info)
